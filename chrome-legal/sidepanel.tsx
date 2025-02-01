7e56e434e318b5937e69e77f7413d5c1cb316941
eqan <eqan.ahmad123@gmail.com>
2025-02-01 16:59:56 +0500

    setting up frontend chrome extension + integerating chatbot with websocket based interface

---
+++ chrome-legal/sidepanel.tsx
@@ -0,0 +1,19 @@
+import App from "./src/App"
+
+function IndexSidePanel() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      <h2>
+      Hi my name is Eqan
+      </h2>
+      <App />
+    </div>
+  )
+}
+
+export default IndexSidePanel
